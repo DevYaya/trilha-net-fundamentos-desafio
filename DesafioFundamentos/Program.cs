6493deaf970fe47acc19d7beab28d0ff8c23b0c3
Yasmin Matos <yasmin1014@hotmail.com>
2022-12-13 21:20:43 -0300

    Conclusão do desafio.

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -3,15 +3,12 @@
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
+Console.Write("Digite o preço inicial: ");
+decimal precoInicial = decimal.Parse(Console.ReadLine());
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Agora digite o preço por hora:");
+decimal precoPorHora = decimal.Parse(Console.ReadLine());
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
